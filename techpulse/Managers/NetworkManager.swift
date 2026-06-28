b2e6f9a71316c661dffd7f120bd90131e9a392ca
_costa <luizfelipeeoliveiraac@gmail.com>
2026-06-28 12:04:51 -0400

    feat: add netowrk manager and fectch news

--- techpulse/Managers/NetworkManager.swift
+++ techpulse/Managers/NetworkManager.swift
@@ -5,4 +5,56 @@
 //  Created by Luiz Felipe on 28/06/26.
 //
 
-import Foundation
+import UIKit
+
+
+class News : Codable {
+    
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    let url = "https://www.tabnews.com.br/api/v1"
+    
+    func fetchTabnewsRecentData(for page: Int , completed: @escaping (Result<[News], TPError>) -> Void) {
+        let endpoint = url + "?&page=1&per_page=100&strategy=newContent"
+        
+        guard let url = URL(string: endpoint) else {
+        completed(.failure(TPError.invalidURL))
+
+         return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            
+            if let _ = error {
+                completed(.failure(TPError.invalidResponse))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let _ = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                let news: [News] = try decoder.decode([News].self, from: data!)
+                completed(.success(news))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }
+        
+        task.resume()
+    }
+}
